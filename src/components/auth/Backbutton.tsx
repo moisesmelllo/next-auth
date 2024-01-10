607dc6ed7462c461f5b3ff02d5e404db523ab5e0
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2024-01-09 22:36:37 -0300

    feat: back button component

---
+++ src/components/auth/Backbutton.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Button } from "../ui/button"
+import Link from "next/link";
+
+interface BackButtonProps {
+  href: string;
+  label: string;
+}
+
+
+export const BackButton = ({
+  label, href
+}: BackButtonProps) => {
+  return (
+   <Button
+    variant={'link'}
+    className="font-normal w-full"
+    size={'sm'}
+    asChild
+   >
+      <Link
+        href={href}
+      >
+        {label}
+      </Link>
+   </Button>
+  )
+}
