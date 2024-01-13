45d0768ec60a212e055be04ce7174112ac545839
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2024-01-13 15:25:46 -0300

    feat: register functionality

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,10 +1,10 @@
-import { LoginForm } from '@/components/auth/LoginForm'
+import { RegisterForm } from '@/components/auth/RegisterForm'
 import React from 'react'
 
 const RegisterPage = () => {
   return (
     <div>
-      <LoginForm />
+      <RegisterForm />
     </div>
   )
 }
---
+++ src/components/auth/RegisterForm.tsx
@@ -0,0 +1,135 @@
+'use client'
+
+import * as z from 'zod'
+
+import { useState, useTransition } from 'react';
+
+import {useForm} from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { RegisterSchema } from '@/schemas';
+import { Input } from '../ui/input';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+import { CardWrapper } from "./CardWrapper";
+import { Button } from '../ui/button';
+import { FormError } from '../form-error';
+import { FormSucess } from '../form-sucess';
+import { register } from '@/actions/register';
+
+export const RegisterForm = () => {
+  const [error, setError] = useState<string |undefined>('')
+  const [success, setSucess] = useState<string |undefined>('')
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      name: '',
+    }
+  })
+
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    setError('')
+    setSucess('')
+    startTransition(() => {
+      register(values)
+      .then((data) => {
+        setError(data.error);
+        setSucess(data.success)
+      })
+    })
+  }
+
+  return (
+    <CardWrapper
+      headerLabel="Create An account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
+      showSocial
+    >
+      <Form {...form}>
+        <form 
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-6'  
+        >
+          <div className='mb-4 space-y-4'>
+          <FormField
+              control={form.control}
+              name='name'
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      {...field}
+                      placeholder='John Doe'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='email'
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      {...field}
+                      placeholder='john.doe@example.com'
+                      type='email'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='password'
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>password</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      {...field}
+                      placeholder='******'
+                      type='password'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error}/>
+          <FormSucess message={success}/>
+          <Button
+            disabled={isPending}
+            type='submit'
+            className='w-full'
+          >
+            Create an account
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+    
+  )
+}
